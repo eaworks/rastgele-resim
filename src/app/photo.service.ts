fa8271ab9a3be456bf4a5db254e7d65e28fa38a5
Ersin Aydin <ersinaydinworks@gmail.com>
2022-12-21 13:03:48 +0300

    istek atma

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+interface CallResponse {
+  urls: {
+    regular: string
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +13,7 @@ export class PhotoService {
 
   constructor(private http: HttpClient) { }
   getPhotos() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
+    return this.http.get<CallResponse>('https://api.unsplash.com/photos/random', {
       headers: { Authorization: 'Client-ID ndYrcIfg1ZviINX7SbYFmyxMQUF8qCVGS2CEGKLXyeU' }
     });
   }
