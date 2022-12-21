d0dfb68b9bb813c73992c6bf23a7379134409382
Ersin Aydin <ersinaydinworks@gmail.com>
2022-12-21 12:45:32 +0300

    unsplash authorization

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -7,4 +7,9 @@ import { Injectable } from '@angular/core';
 export class PhotoService {
 
   constructor(private http: HttpClient) { }
+  getPhotos() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: { Authorization: 'Client-ID ndYrcIfg1ZviINX7SbYFmyxMQUF8qCVGS2CEGKLXyeU' }
+    });
+  }
 }
